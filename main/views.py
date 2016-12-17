4176a93cb9335a173574c7f26d835048e9dc41a3
Mina Gaid <mina94@hotmail.co.uk>
2016-12-17 17:19:08 +0000

    Login
    Added login system

--- main/views.py
+++ main/views.py
@@ -1,65 +1,25 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.views.generic import View
-from .forms import UserForm
+from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
-
+	return render(request, 'main/index.html')
+	
 def about(request):
-    return render(request, 'main/about.html')
+	return render(request, 'main/about.html')
 
 def login(request):
-    return render(request, 'main/login.html')
-	
-def logout(request):
-    return render(request, 'main/index.html')
+	return render(request, 'main/login.html')
 
 def preset(request):
-    return render(request, 'main/password-reset.html')
-
+	return render(request, 'main/password-reset.html')
+	
 def signup(request):
-    return render(request, 'main/signup.html')
+	return render(request, 'main/signup.html')
 
 def support(request):
-    return render(request, 'main/faq.html')
-
+	return render(request, 'main/faq.html')
+	
 def contact(request):
-    return render(request, 'main/contact.html')
-
+	return render(request, 'main/contact.html')
+	
 def terms(request):
-    return render(request, 'main/terms-and-conditions.html')
-
-class UserFormView(View):
-	form_class = UserForm
-	template_name = 'main/signup.html'
-
-	# display blank form
-	def get(self, request):
-		form = self.form_class(None)
-		return render(request, self.template_name, {'form': form})
-
-    # process form data
-	def post(self, request):
-            form = self.form_class(request.POST)
-
-            if form.is_valid():
-                user = form.save(commit=False)
-                # cleaned (normalized) data
-                username = form.cleaned_data['username']
-                password = form.cleaned_data['password']
-                user.set_password(password)
-                user.save()
-
-                # returns User objects if credentials are correct
-                user = authenticate(username=username, password=password)
-
-                if user is not None:
-                    if user.is_active:
-                        login(request, user)
-                        return render(request, 'main/index.html')
-
-            return render(request, self.template_name, {'form': form})
-
-
-
+	return render(request, 'main/terms-and-conditions.html')

a460a0b76cc51cb0fc6123f8943dbe9f695d0185
Mina Gaid <mina94@hotmail.co.uk>
2016-12-18 13:28:35 +0000

    commit

--- dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from . import views
-
-urlpatterns = [
-	url(r'^control-panel/$', views.panel, name='control-panel'),
-	url(r'^tables/$', views.tables, name='tables'),
-	url(r'^charts/$', views.charts, name='charts'),
-]
